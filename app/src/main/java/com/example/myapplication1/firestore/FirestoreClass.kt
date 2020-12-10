bce384c5e14fa91029b298b3445ab97d65a0d714
Ivan-Ramadhan <ivanzramadhanz@gmail.com>
2020-12-11 03:32:09 +0700

    Setup Firestore

---
+++ app/src/main/java/com/example/myapplication1/firestore/FirestoreClass.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication1.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreClass {
+
+    private val mFireStore = FirebaseFirestore.getInstance()
+
+}
