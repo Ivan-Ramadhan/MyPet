1fbe499f3bc45b0051cf991be332a6d928764f97
Ivan-Ramadhan <ivanzramadhanz@gmail.com>
2020-12-12 07:14:12 +0700

    Load image with Glide Loader

--- app/src/main/java/com/example/myapplication1/UserProfileActivity.kt
+++ app/src/main/java/com/example/myapplication1/UserProfileActivity.kt
@@ -1,17 +1,27 @@
 package com.example.myapplication1
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myapplication1.models.User
 import com.example.myapplication1.utils.Constants
+import com.example.myapplication1.utils.GlideLoader
 import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.io.IOException
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
 
-        // TODO Step 5: Retrieve the User details from intent extra.
+
         // START
         // Create a instance of the User model class.
         var userDetails: User = User()
@@ -21,7 +31,7 @@ class UserProfileActivity : AppCompatActivity() {
         }
         // END
 
-        // TODO Step 6: After receiving the user details from intent set it to the UI.
+
         // START
         // Here, the some of the edittext components are disabled because it is added at a time of Registration.
         et_first_name.isEnabled = false
@@ -34,5 +44,103 @@ class UserProfileActivity : AppCompatActivity() {
         et_email.setText(userDetails.email)
         // END
 
+        iv_user_photo.setOnClickListener(this@UserProfileActivity)
+
     }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                R.id.iv_user_photo -> {
+
+                    // Here we will check if the permission is already allowed or we need to request for it.
+                    // First of all we will check the READ_EXTERNAL_STORAGE permission and if it is not allowed we will request for the same.
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Constants.showImageChooser(this)
+
+//                        showErrorSnackBar("You already have the storage permission.", false)
+                    } else {
+
+                        /*Requests permissions to be granted to this application. These permissions
+                         must be requested in your manifest, they should not be granted to your app,
+                         and they should have protection level*/
+
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+//
+                Constants.showImageChooser(this)
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.PICK_IMAGE_REQUEST_CODE) {
+                if (data != null) {
+                    try {
+                        // The uri of selected image from phone storage.
+                        val selectedImageFileUri = data.data!!
+
+//                        iv_user_photo.setImageURI(selectedImageFileUri)
+                        GlideLoader(this@UserProfileActivity).loadUserPicture(
+                            selectedImageFileUri,
+                            iv_user_photo
+                        )
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(
+                            this@UserProfileActivity,
+                            resources.getString(R.string.image_selection_failed),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            // A log is printed when user close or cancel the image selection.
+            Log.e("Request Cancelled", "Image selection cancelled")
+        }
+    }
+
 }
--- app/src/main/java/com/example/myapplication1/utils/Constants.kt
+++ app/src/main/java/com/example/myapplication1/utils/Constants.kt
@@ -1,5 +1,9 @@
 package com.example.myapplication1.utils
 
+import android.app.Activity
+import android.content.Intent
+import android.provider.MediaStore
+
 object Constants {
 
     // Firebase Constants
@@ -11,4 +15,17 @@ object Constants {
 
     const val EXTRA_USER_DETAILS: String = "extra_user_details"
 
+    const val READ_STORAGE_PERMISSION_CODE = 2
+
+    const val PICK_IMAGE_REQUEST_CODE = 1
+
+    fun showImageChooser(activity: Activity) {
+        // An intent for launching the image selection of phone storage.
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        // Launches the image selection of phone storage using the constant code.
+        activity.startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
 }
