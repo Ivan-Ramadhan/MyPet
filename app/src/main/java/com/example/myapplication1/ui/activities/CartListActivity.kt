6c3e98a97f9828f9b498695f6fac78e2b932498a
Ivan-Ramadhan <ivanzramadhanz@gmail.com>
2020-12-27 16:14:58 +0700

    Add Function to Get Item Cart Data from Firestore & Add item cart layout xml for showing item in cart list later.

--- app/src/main/java/com/example/myapplication1/ui/activities/CartListActivity.kt
+++ app/src/main/java/com/example/myapplication1/ui/activities/CartListActivity.kt
@@ -1,12 +1,53 @@
 package com.example.myapplication1.ui.activities
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import com.example.myapplication1.R
+import com.example.myapplication1.firestore.FirestoreClass
+import com.example.myapplication1.models.CartItem
+import kotlinx.android.synthetic.main.activity_cart_list.*
 
-class CartListActivity : AppCompatActivity() {
+class CartListActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart_list)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        getCartItemsList()
+    }
+
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_cart_list_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+        }
+
+        toolbar_cart_list_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun getCartItemsList() {
+
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        FirestoreClass().getCartList(this@CartListActivity)
+    }
+
+
+    fun successCartItemsList(cartList: ArrayList<CartItem>) {
+
+        hideProgressDialog()
+
+        for (i in cartList) {
+
+            Log.i("Cart Item Title", i.title)
+
+        }
     }
 }
